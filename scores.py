143c22dbe88191c86f2029a66fd080274e4a3954
liam <liamstokes@ufl.edu>
2020-12-05 18:15:14 -0500

    added a scorelist

--- scores.py
+++ scores.py
@@ -1,7 +1,6 @@
 class scores(object):
 
-  def__init__(self, score1, score2, score3):
-  
+  def __init__(self, score1, score2, score3):
     self.score1 = score1
     self.score2 = score2
     self.score3 = score3
